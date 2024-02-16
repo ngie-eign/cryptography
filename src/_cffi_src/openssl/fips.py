75191f0797eed6424bc2e5e1011b0061d9f5c58e
Enji Cooper <yaneurabeya@gmail.com>
2024-02-15 18:46:54 -0800

    Add OpenSSL 3.0 support to 3.3.x
    Conflicts:
    	src/_cffi_src/openssl/cryptography.py
    	src/_cffi_src/openssl/rsa.py

--- src/_cffi_src/openssl/fips.py
+++ src/_cffi_src/openssl/fips.py
@@ -6,23 +6,50 @@ from __future__ import absolute_import, division, print_function
 
 INCLUDES = """
 #include <openssl/crypto.h>
+#include <openssl/evp.h>
 """
 
 TYPES = """
-static const long Cryptography_HAS_FIPS;
+static const long Cryptography_HAS_300_FIPS;
+static const long Cryptography_HAS_LEGACY_FIPS;
 """
 
 FUNCTIONS = """
-int FIPS_mode_set(int);
-int FIPS_mode(void);
 """
 
 CUSTOMIZATIONS = """
+#ifndef OSSL_LIB_CTX
+/**
+ * HACK: OSSL_LIB_CTX is only defined on 3.x+.
+ *
+ * Fake it on non-3.x versions with a bogus #define (deviates less than using a
+ * typedef).
+ */
+#define OSSL_LIB_CTX    void
+#endif
+
 #if CRYPTOGRAPHY_IS_LIBRESSL
+static const long Cryptography_HAS_300_FIPS = 0;
 static const long Cryptography_HAS_FIPS = 0;
-int (*FIPS_mode_set)(int) = NULL;
+
+int (*EVP_default_properties_enable_fips)(OSSL_LIB_CTX *, int) = NULL;
+int (*EVP_default_properties_is_fips_enabled)(OSSL_LIB_CTX *) = NULL;
 int (*FIPS_mode)(void) = NULL;
-#else
-static const long Cryptography_HAS_FIPS = 1;
+int (*FIPS_mode_set)(int) = NULL;
+
+# elif CRYPTOGRAPHY_OPENSSL_300_OR_GREATER
+/* Provide a 1.x/3.x version agnostic set of FIPS APIs. */
+static const long Cryptography_HAS_300_FIPS = 1;
+static const long Cryptography_HAS_LEGACY_FIPS = 0;
+
+int (*FIPS_mode)(void) = NULL;
+int (*FIPS_mode_set)(int) = NULL;
+
+# else
+static const long Cryptography_HAS_300_FIPS = 0;
+static const long Cryptography_HAS_LEGACY_FIPS = 1;
+
+int (*EVP_default_properties_enable_fips)(OSSL_LIB_CTX *, int) = NULL;
+int (*EVP_default_properties_is_fips_enabled)(OSSL_LIB_CTX*) = NULL;
 #endif
 """
--- src/_cffi_src/openssl/rsa.py
+++ src/_cffi_src/openssl/rsa.py
@@ -44,7 +44,7 @@ int EVP_PKEY_CTX_set_rsa_pss_saltlen(EVP_PKEY_CTX *, int);
 int EVP_PKEY_CTX_set_rsa_mgf1_md(EVP_PKEY_CTX *, EVP_MD *);
 int EVP_PKEY_CTX_set0_rsa_oaep_label(EVP_PKEY_CTX *, unsigned char *, int);
 
-int EVP_PKEY_CTX_set_rsa_oaep_md(EVP_PKEY_CTX *, EVP_MD *);
+int EVP_PKEY_CTX_set_rsa_oaep_md(EVP_PKEY_CTX *, const EVP_MD *);
 """
 
 CUSTOMIZATIONS = """
